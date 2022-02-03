b8ac81eccda91f0795b867337b74ad73da5926a3
nyandago <paulmutator@gmail.com>
2022-02-03 12:08:04 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,20 +32,88 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.black,
+      appBar:
+          AppBar(title: Text(widget.title), backgroundColor: Colors.blueAccent),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Expanded(child: Text('This is the Question')),
-            Column(
-              children: [
-                TextButton(onPressed: () {}, child: const Text('True')),
-                TextButton(onPressed: () {}, child: const Text('False')),
+            const Expanded(
+                child: Text(
+              'This is the Question',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+            Column(children: [
+              Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFF64DD17),
+                            Color(0xFF64DD17),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text('TRUE'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFF64DD17),
+                            Color(0xFF64DD17),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text('FALSE'),
+                  ),
+                ],
+              ),
+            ]),
+            Row(
+              children: const [
+                Icon(
+                  Icons.check,
+                  color: Colors.green,
+                ),
+                Icon(
+                  Icons.check,
+                  color: Colors.red,
+                )
               ],
-            )
+            ),
           ],
         ),
       ),
