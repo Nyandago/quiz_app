b2ace4ebd17353a947f325b74e53f991a55ecb51
nyandago <paulmutator@gmail.com>
2022-02-03 15:43:35 +0300

    update question.dart

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question(String q, bool a) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
